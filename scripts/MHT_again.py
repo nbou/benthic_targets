b816dda1510e18f2e93e16bd48ce339e7b531f44
Nader Boutros <N.Boutros@aims.gov.au>
2023-06-03 17:00:54 +1000

    backup from trip

---
+++ scripts/MHT_again.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from src.utils import get_last_detection_id
+def update_hyps(det, hyps):
+    new_hyps = []
+
+    detection_id = det.iloc[0].id
+
+    # go through hypotheses
+    for hyp_i, hyp in enumerate(hyps):
+
+
+        # scenario where the new detection is a new individual
+        # pad existing branches
+
+        new_scen = [h + [np.nan] for h in hyp] + [list(np.empty(len(hyp[0])) * np.nan) + [detection_id]]
+        new_hyps.append(new_scen)
+
+
+        # go through the branches in the hypothesis
+        for i, branch in enumerate(hyp):
+            # calculate score
+            last_detection = get_last_detection_id(branch)
+            repeat_scen = [branch + [detection_id]]
+            repeat_scen += [hyp[j] + [np.nan] for j in range(len(hyp)) if j != i]
+
+            new_hyps.append(repeat_scen)
+
+
+    return new_hyps
+
+
+
+if __name__ == "__main__":
+    var = 0.01
+
+    detections = pd.DataFrame(columns=["id", "time", "x"])
+    detections.id = [0,1,2]
+    detections.time = [0,1,2]
+    detections.x = [0, 1, 1.01]
+
+    # fig, ax = plt.subplots()
+    # ax.scatter(detections.x, detections.time)
+    # plt.show()
+
+    # t=0
+    det = detections[detections.time == 0]
+    hyps = [[[0]]]
+
+    # t=1
+    i=1
+    det = detections[detections.time == i]
+    hyps = update_hyps(det, hyps)
+
+
+    a = 10
